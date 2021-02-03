7c5bd60797a2a69a3d8f2897b348697c5660add4
Matthew Smith <matthew.smith@dal.ca>
2021-02-03 19:58:42 -0400

    add comments

--- test/test_segments.py
+++ test/test_segments.py
@@ -14,7 +14,11 @@ assert sys.version_info.major > 3 or sys.version_info.minor >= 8, 'python >= 3.8
 
 def test_track_segments():
     # load test dataset
+    # note that rows are sorted by mmsi and time, as output by the database
     with open('test/testdata/longtrack.pickle', 'rb') as f: testrows = pickle.load(f)
+
+    # TODO: update track segment function to allow a variable number of columns
+    # currently requires rows to have columns: mmsi time lon lat cog sog vessel_name vessel_type
 
     '''
     >>> testrows 
@@ -28,12 +32,9 @@ def test_track_segments():
           dtype=object)
     '''
 
-    # TODO: update track segment function to allow a variable number of columns
-    # currently requires rows to have columns: mmsi time lon lat cog sog vessel_name vessel_type
-
     # segment function parameters
-    maxdelta    = timedelta(minutes=30)    # time threshold to allow before segmenting tracks
-    minsize     = 1                     # minimum segment length
+    maxdelta    = timedelta(minutes=30)     # time threshold to allow before segmenting tracks
+    minsize     = 1                         # minimum segment length
 
     # each segment can then be filtered for e.g. duplicate timestamps, max speed, etc..
     filters = [
@@ -50,7 +51,7 @@ def test_track_segments():
             mask = filtermask(track, rng, filters)  # filters are applied by passing filters to filtermask function
             print(f"segment: {rng}\tsegment length: {len(rng)}\tfiltered: {len(rng)-sum(mask)}\tstart time: {track['time'][rng][0]}\t\tend time: {track['time'][rng][-1]}")
 
-    # or, more concisely (for datasets that fit in memory)
+    # or, more concisely without filtering (for datasets that fit in memory)
     tracks_segmented = { track['mmsi'] : dict(**track, seg=list(segment(track, maxdelta, minsize))) for track in trackgen(testrows) }
     
     breakpoint()
