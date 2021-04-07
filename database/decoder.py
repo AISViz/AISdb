c5c1d51bdaad2145406f2250f17be0716bd21e3f
matt24smith <matt24smith@gmail.com>
2021-04-07 12:58:55 -0300

    switch decoder to libais and adapt to new data format

--- database/decoder.py
+++ database/decoder.py
@@ -3,13 +3,14 @@ from datetime import datetime, timedelta
 import re
 import sqlite3
 
-from pyais import FileReaderStream
+#from pyais import FileReaderStream
+from ais import decode
 import numpy as np
 
 from database.create_tables import *
 
 
-datestr = re.compile('[0-9]{4}-[0-9]{2}-[0-9]{2}|[0-9]{8}')
+#datestr = re.compile('[0-9]{4}-[0-9]{2}-[0-9]{2}|[0-9]{8}')
 
 
 def is_valid_date(year, month, day, hour=0, minute=0, second=0):
@@ -22,12 +23,20 @@ def is_valid_date(year, month, day, hour=0, minute=0, second=0):
 def decode_csv(fpath, conn, mstr=None):
     cur = conn.cursor()
 
-    mstr = fpath.rsplit('_',1)[1].rsplit('-',1)[0].replace('-','')
-    assert int(mstr) >= 201101
-    regexdate = datestr.search(fpath)
-    filedate = datetime(*map(int, [(dstr := fpath[regexdate.start():regexdate.end()].replace('-', ''))[0:4], dstr[4:6], dstr[6:8]]))
-    maxdelta = timedelta(hours=36)
-    
+    with open(fpath, 'r') as f:
+        header = f.readline().rstrip('\n').lstrip('"').rstrip('"').split('","')
+        msgs = np.array([{k:v for k,v in zip(header, msg.rstrip('\n').lstrip('"').rstrip('"').split('","'))} for msg in f.readlines()])
+
+    msgs = msgs[np.array([msg['Date time stamp'] for msg in msgs]) != '']
+
+    payloads = np.array([msg['Iec message'] for msg in msgs])
+
+    #aivdm, count, fragment, seqid, channel, payload, fill = [msg['Iec message'].split('|')[0].split(',') for msg in msgs]
+    #list(map(chr, [char if char-48 <= 40 else char-8 for char in map(ord, fill)]))
+
+    stamps = np.array([datetime.strptime(msg['Date time stamp'], '%Y-%m-%d %H:%M:%S UTC') for msg in msgs], dtype=datetime)
+    np.array([msg['Date time stamp'] for msg in msgs]) != ''
+    mstr = str(f'{stamps[0].year:04d}{stamps[0].month:02d}')
 
     cur.execute(f'SELECT name FROM sqlite_master WHERE type="table" AND name="ais_s_{mstr}_msg_1_2_3" ')
     if not cur.fetchall():
@@ -38,126 +47,88 @@ def decode_csv(fpath, conn, mstr=None):
         create_table_msg27(cur, mstr)
         create_table_msg_other(cur, mstr)
 
-    rows = np.array([msg.decode().content for msg in FileReaderStream(fpath) if msg.decode() is not None], dtype=object)
-
-    rowtypes = [row['type'] for row in rows]
-    '''
-    2018-01-28 only 1000 msg123 ???
-    fpath = sorted(csvfiles)[24]
-    fpath
-    { m : rowtypes.count(m) for m in np.unique(rowtypes) }
-    m4 = np.array([r['type'] == 4 for r in rows])
-    mtest = np.array([r['type'] == 8 or r['type'] == 24 for r in rows])
-    rows[mtest]
-    '''
-
-    if len(rows) == 0:
-        print('no data! skipping...')
-        return
-
-    stamp = datetime(1,1,1)
-    rowiter = iter(rows)
-    r = next(rowiter)
-    while stamp.year < 2000:
-        while 'year' not in r.keys() or r['year'] < 2000 or not 0 <= r['second'] <= 60: r = next(rowiter) 
-        stamp = datetime(r['year'], r['month'], r['day'], r['hour'], r['minute'], r['second'])
-    year, month, day, hour, minute, second = stamp.year, stamp.month, stamp.day, stamp.hour, stamp.minute, stamp.second
-
-    stamps = []
-    for row in rows:
-        if 1 <= row['type'] <= 3 and row['second'] < 60:
-            second = row['second']
-        elif (row['type'] == 4 
-                and is_valid_date(*(datecols := (row['year'], row['month'], row['day'], row['hour'], row['minute'], row['second'])))
-                and filedate - datetime(*datecols) < maxdelta):
-            year, month, day = row['year'], row['month'], row['day']
-            hour, minute, second = row['hour'], row['minute'], row['second']
-        elif (row['type'] == 5 
-                and is_valid_date(*(datecols := (year, row['month'], row['day'], row['hour'], row['minute']))) 
-                and filedate - datetime(*datecols) < maxdelta):
-            month, day, hour, minute = row['month'], row['day'], row['hour'], row['minute']
-        elif (row['type'] == 11 
-                and is_valid_date(*(datecols := (row['year'], row['month'], row['day'], row['hour'], row['minute'], row['second'])))
-                and filedate - datetime(*datecols) < maxdelta):
-            year, month, day = row['year'], row['month'], row['day']
-            hour, minute, second = row['hour'], row['minute'], row['second']
-        elif (row['type'] == 18 
-                and row['second'] < 60):
-            second = row['second']
-        elif row['type'] == 24: 
-            pass
-        stamps.append(datetime(year, month, day, hour, minute, second))
-
-    stamps = np.array(stamps, dtype=object).astype(datetime)
 
     # insert messages 1,2,3
-    m123 = np.array([0 < r['type'] < 4 and not (r['lat'] == r['lon'] == 0 ) for r in rows])
-    tup123 = ((
-                r['type'], r['repeat'], int(r['mmsi']), r['status'].value, r['turn'], 
-                r['speed'], r['accuracy'], r['lon'], r['lat'], r['course'], r['heading'], 
-                r['second'], r['maneuver'].value, r['raim'], r['radio'], 
-                datetime(t.year, t.month, t.day, t.hour, r['second'] if r['second'] < 60 else 0),
-                f'''POINT (({r['lon']}, {r['lat']}))''', 
-            ) for r,t in zip(rows[m123], stamps[m123])
-        )
-
-    cur.executemany(f'INSERT OR IGNORE INTO ais_s_{mstr}_msg_1_2_3 '
-                    '(message_id, repeat_indicator, mmsi, navigational_status, rot, '
-                    'sog, accuracy, longitude, latitude, cog, heading, utc_second, '
-                    'maneuver, raim_flag, communication_state, time, ais_geom) '
-                    '''VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,GeomFromText(?, 4326))''', tup123)
+    if 'ITU123' in fpath:
+        rows = np.array([decode(payload.split(',')[5], 0) for payload in payloads])
+        m123 = np.array([0 < r['id'] < 4 for r in rows])
+        tup123 = ((
+                    r['id'], r['repeat_indicator'], int(r['mmsi']), r['nav_status'], r['rot'], 
+                    r['sog'], r['position_accuracy'], r['x'], r['y'], r['cog'], r['true_heading'], 
+                    t.second, r['special_manoeuvre'], r['raim'], r['sync_state'], 
+                    datetime(t.year, t.month, t.day, t.hour, t.second if t.second < 60 else 0),
+                    f'''POINT (({r['x']}, {r['y']}))''', 
+                ) for r,t in zip(rows[m123], stamps[m123])
+            )
+        cur.executemany(f'INSERT OR IGNORE INTO ais_s_{mstr}_msg_1_2_3 '
+                        '(message_id, repeat_indicator, mmsi, navigational_status, rot, '
+                        'sog, accuracy, longitude, latitude, cog, heading, utc_second, '
+                        'maneuver, raim_flag, communication_state, time, ais_geom) '
+                        '''VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,GeomFromText(?, 4326))''', tup123)
 
     # insert message 5
-    m5 = np.array([r['type'] == 5 for r in rows])
-    tup5 = ((
-        r['type'], r['repeat'], r['mmsi'], r['ais_version'] , r['imo'], r['callsign'], 
-        r['shipname'], r['shiptype'], r['to_bow'], r['to_stern'], r['to_port'], 
-        r['to_starboard'], r['epfd'].value, r['month'], r['day'], r['hour'], r['minute'], 
-        r['draught'], r['destination'], r['dte']) for r in rows[m5])
+    elif 'ITU5' in fpath:
+        #m5 = np.array([r['id'] == 5 for r in rows])
+        m5 = np.array([True for _ in msgs])
+        tup5 = ((
+                    r['Msg type'], None, r['MMSI'], None , r['IMO number'], r['Callsign'], 
+                    r['Name'].rstrip(), r['Type of ship'], r['DimensionA'], r['DimensionB'], r['DimensionC'], 
+                    r['DimensionD'], r['Electronic fixing device'], int(r['Eta'][0:2]), int(r['Eta'][2:4]), 
+                    int(r['Eta'][4:6]), int(r['Eta'][6:8]), r['Max draught'], r['Destination'], 1) 
+                for r,t in zip(msgs[m5], stamps[m5]))
 
-    cur.executemany(f'INSERT OR IGNORE INTO ais_s_{mstr}_msg_5 '
-                    '(message_id, repeat_indicator, mmsi, ais_version, imo, call_sign, '
-                    'vessel_name, ship_type, dim_bow, dim_stern, dim_port, dim_star, '
-                    'fixing_device, eta_month, eta_day, eta_hour, eta_minute, draught, '
-                    'destination, dte) '
-                    'VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', tup5)
+        cur.executemany(f'INSERT OR IGNORE INTO ais_s_{mstr}_msg_5 '
+                        '(message_id, repeat_indicator, mmsi, ais_version, imo, call_sign, '
+                        'vessel_name, ship_type, dim_bow, dim_stern, dim_port, dim_star, '
+                        'fixing_device, eta_month, eta_day, eta_hour, eta_minute, draught, '
+                        'destination, dte) '
+                        'VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', tup5)
 
     # insert message 18, 19
-    m18 = np.array([r['type'] == 18 and not (r['lat'] == r['lon'] == 0 ) for r in rows])
-    tup18 = ((
-                r['type'], r['repeat'], r['mmsi'], r['speed'] , r['accuracy'], r['lon'], 
-                r['lat'], r['course'], r['heading'], r['second'], r['regional'], r['cs'] if 'cs' in r.keys() else False, 
-                r['display'], r['dsc'], r['band'], r['msg22'], r['assigned'], r['raim'], 
-                r['radio'],
-                datetime(t.year, t.month, t.day, t.hour, r['second'] if r['second'] < 60 else 0),
-                f'''POINT (({r['lon']}, {r['lat']}))''', 
-            ) for r,t in zip(rows[m18], stamps[m18])
-        )
+    elif 'ITU18' in fpath:
+        rows = np.array([decode(payload.split(',')[5], 0) for payload in payloads])
+        m18 = np.array([r['id'] == 18 for r in rows])
+        tup18 = ((
+                    r['id'], r['repeat_indicator'], r['mmsi'], r['sog'] , r['position_accuracy'], r['x'], 
+                    r['y'], r['cog'], r['true_heading'], r['timestamp'], None, r['commstate_flag'], 
+                    r['display_flag'], r['dsc_flag'], r['band_flag'], r['m22_flag'], r['mode_flag'], r['raim'], 
+                    r['commstate_cs_fill'] if 'commstate_cs_fill' in r.keys() else None,
+                    datetime(t.year, t.month, t.day, t.hour, r['timestamp'] if r['timestamp'] < 60 else 0),
+                    f'''POINT (({r['x']}, {r['y']}))''', 
+                ) for r,t in zip(rows[m18], stamps[m18])
+            )
 
-    cur.executemany(f'INSERT OR IGNORE INTO ais_s_{mstr}_msg_18 '
-                    '(message_id, repeat_indicator, mmsi, sog, accuracy, longitude, '
-                    'latitude, cog, heading, utc_second, region, communication_flag, '
-                    'display, dsc, band, msg22, mode, raim_flag, communication_state, time, ais_geom) '
-                    'VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,GeomFromText(?, 4326))', tup18)
+        cur.executemany(f'INSERT OR IGNORE INTO ais_s_{mstr}_msg_18 '
+                        '(message_id, repeat_indicator, mmsi, sog, accuracy, longitude, '
+                        'latitude, cog, heading, utc_second, region, communication_flag, '
+                        'display, dsc, band, msg22, mode, raim_flag, communication_state, time, ais_geom) '
+                        'VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,GeomFromText(?, 4326))', tup18)
 
     # insert message 24
-    m24 = np.array([r['type'] == 24 for r in rows])
-    tup24 = ((
-        r['type'], r['repeat'], r['mmsi'], (p := r['partno']), r['shiptype'].value if p else None, 
-        r['vendorid'] if p else None, r['model'] if p else None, r['serial'] if p else None, 
-        r['callsign'] if p else None, r['to_bow'] if p else None, r['to_stern'] if p else None, 
-        r['to_port'] if p else None, r['to_starboard'] if p else None, 
-        r['mothership_mmsi'] if p else None, 
-        ) for r in rows[m24])
+    elif 'ITU24' in fpath:
+        #rows = np.array([decode(payload.split(',')[5], 0) for payload in payloads])
+        #rows = np.array([decode(payload.split(',')[5], int(payload.split(',')[6][0])) for payload in payloads])
+        m24 = np.array([int(r['Msg type']) == 24 for r in msgs])
+        tup24 = ((
+            int(r['Msg type']), None, int(r['MMSI']), None, int(r['Type of ship'][1:3]) if not r['Type of ship'] == '' and r['Type of ship'][0] == '[' else None, 
+            None, None, None, 
+            r['Callsign'], r['DimensionA'] , r['DimensionB'], r['DimensionC'], r['DimensionD'], 
+            None, 
+            ) for r in msgs[m24])
 
-    cur.executemany(f'INSERT OR IGNORE INTO ais_s_{mstr}_msg_24 '
-                    '(message_id, repeat_indicator, mmsi, sequence_id, ship_type, vendor_id,  '
-                    'model, serial, call_sign, dim_bow, dim_stern, dim_port, dim_star, mother_ship_mmsi) '
-                    'VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)', tup24)
+        cur.executemany(f'INSERT OR IGNORE INTO ais_s_{mstr}_msg_24 '
+                        '(message_id, repeat_indicator, mmsi, sequence_id, ship_type, vendor_id,  '
+                        'model, serial, call_sign, dim_bow, dim_stern, dim_port, dim_star, mother_ship_mmsi) '
+                        'VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)', tup24)
+
+    else:
+        print(f'skipped {fpath}')
 
     conn.commit()
-    m19 = np.array([r['type'] == 19 for r in rows])
-    if sum(m19) > 0: print(f'skipped message 19: {sum(m19)}')
+    #m19 = np.array([r['id'] == 19 for r in rows])
+    #if sum(m19) > 0: print(f'skipped message 19: {sum(m19)}')
+    #m27 = np.array([r['id'] == 27 for r in rows])
+    #if sum(m27) > 0: print(f'skipped message 27: {sum(m27)}')
 
 
 
@@ -165,17 +136,18 @@ if __name__ == '__main__':
 
     folder = "/run/media/matt/Seagate Backup Plus Drive/CCG_Terrestrial_AIS_Network/Raw_data/2018/"
     glob  = list(os.walk(folder))
-    csvfiles = [glob[0][0] + f for f in glob[0][2] if f[-4:] == '.csv']
+    csvfiles = [os.path.join(glob[0][0],f) for f in glob[0][2] if f[-4:] == '.csv']
+    csvfiles2 = [os.path.join(glob[1][0],f) for f in glob[1][2] if f[-4:] == '.csv']
 
     # assuming WGS84 (EPSG 4326)
 
     # 28k bad timestamps, 5k good timestamps, 98k incomplete
     # should invalid timestamps be fixed and keep lon/lat or discarded?
     fpath = "/run/media/matt/Seagate Backup Plus Drive/CCG_Terrestrial_AIS_Network/Raw_data/2018/CCG_AIS_Log_2018-01-21.csv"
-
+    conn = dbconn('/run/media/matt/Seagate Backup Plus Drive/python/ais.db').conn
 
     t1 = datetime.now()
-    for fpath in sorted(csvfiles)[28:]: 
+    for fpath in sorted(csvfiles): 
         print(fpath)
         decode_csv(fpath, conn)
         regexdate = datestr.search(fpath)
