889cc5fc43cf9af8a2c22b3c05c3c53dfab07031
matt24smith <matt24smith@gmail.com>
2021-07-05 12:15:43 -0300

    free shore distance raster from memory after loading data

--- shore_dist.py
+++ shore_dist.py
@@ -1,19 +1,28 @@
 import os
 import rasterio 
 
-
 class shore_dist_gfw():
     '''
     the raster data file used here can be downloaded from:
     https://globalfishingwatch.org/data-download/datasets/public-distance-from-port-v1
-    '''
 
-    def __init__(self, rasterfile='input/distance-from-port-v20201104.tiff'):
+    TODO: copy file download script from gebco.py
+    '''
+    # load raster into memory
+    def __enter__(self, rasterfile='input/distance-from-port-v20201104.tiff'):
         assert os.path.isfile(rasterfile)
         self.dataset = rasterio.open(rasterfile)
         self.band1 = self.dataset.read(1)
+        return self
 
+    # cleanup resources on exit
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.dataset.close()
+        self.dataset.stop()
+        del self.dataset
+        del self.band1
 
+    # get approximate shore distance for coordinates (kilometres)
     def getdist(self, lon, lat):
         ixlon, ixlat = self.dataset.index(lon, lat)
         return self.band1[ixlon,ixlat]
