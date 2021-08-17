734e881cca1bed3d51f4f3ed016d7c9757a111e7
matt24smith <matt24smith@gmail.com>
2021-08-17 14:24:20 -0300

    improved geometry calculation and paralellization

--- network_graph.py
+++ network_graph.py
@@ -9,7 +9,7 @@ import numpy as np
 from shapely.geometry import Point, LineString, Polygon
 
 
-from gis import delta_knots, delta_meters
+from gis import delta_knots, delta_meters, delta_seconds, ZoneGeom, Domain
 from database import dt2monthstr, dbconn, epoch_2_dt
 from track_gen import trackgen, segment, filtermask, writecsv
 from shore_dist import shore_dist_gfw
@@ -34,14 +34,15 @@ time_in_shoredist_rng = lambda track, subset, dist0=0.01, dist1=5: (
 )
 
 # collect vessel track statistics
-segmentinfo = lambda track, stacked_arr, src_zone, domain: dict(
+segmentinfo = lambda track, stacked_arr, src_zone, domainname: dict(
         mmsi                                =   track['mmsi'],
         imo                                 =   track['imo'] or '',
         vessel_name                         =   track['vessel_name'] or '',
         vessel_type                         =   track['ship_type_txt'] or '',
-        domain                              =   domain,
+        domainname                          =   domainname,
         src_zone                            =   src_zone,
         rcv_zone                            =   'NULL',
+        transit_nodes                       =   'NULL',
         vessel_length                       =   (track['dim_bow'] + track['dim_stern']) or '',
         hull_submerged_surface_area         =   track['submerged_hull_m^2'] or '',
         ballast                             =   None,
@@ -79,15 +80,12 @@ zone_stats = lambda track, zoneset: dict(
 
 
 
-def merge_layers(rowgen, zones, dbpath):
+def merge_layers(rowgen, dbpath):
     ''' generator function to merge AIS row data with shore distance, bathymetry, and geometry databases
 
         args:
             rowgen: generator function 
                 yields sets of rows grouped by MMSI sorted by time
-            zones: dictionary
-                see zones_from_txts() function in gis.py
-                returns geometry objects as dictionary values
             dbpath: string
                 path to .db file
 
@@ -107,13 +105,10 @@ def merge_layers(rowgen, zones, dbpath):
     with shore_dist_gfw(dbpath=dbpath) as sdist, Gebco(dbpath=dbpath) as bathymetry, marinetraffic.scrape_tonnage(dbpath=dbpath) as hullgeom:
 
         for rows in rowgen:
-            #rows = np.array(list(rowgen))
-
             xy = rows[:,2:4]
             mmsi_column, imo_column, ship_type_column = 0, 7, 13
 
             # vessel geometry
-            #print('aggregating unique mmsi, imo...')
             uniqueID = {}
             _ = [uniqueID.update({f'{r[mmsi_column]}_{r[imo_column]}' : {'m' : r[mmsi_column], 'i' : r[imo_column]}}) for r in rows]
 
@@ -132,40 +127,32 @@ def merge_layers(rowgen, zones, dbpath):
             submerged_hull = np.array([wsa(d, r) for d,r in zip(deadweight_tonnage,ship_type) ])
 
             # shore distance from cell grid
-            #print('loading shore distance...')
             km_from_shore = np.array([sdist.getdist(x, y) for x, y in xy ])
 
             # seafloor depth from cell grid
-            #print('loading bathymetry...')
             depth = np.array([bathymetry.getdepth(x, y) for x,y in xy ]) * -1
 
             yield np.hstack((rows, np.vstack((deadweight_tonnage, submerged_hull, km_from_shore, depth)).T))
-        #print('merging...')
-        #merged = np.hstack((rows, np.vstack((deadweight_tonnage, submerged_hull, km_from_shore, depth)).T))
-
-    #return merged
 
 
-def geofence(track_merged, zones, dbpath, colnames, 
+def geofence(track_merged, domain, dbpath, colnames, 
         maxdelta=timedelta(hours=1)
         ,
-        filters=[
-            lambda track, rng: [True for _ in rng[:-1]],
-            ]
+        #filters=[
+        #    lambda track, rng: [True for _ in rng[:-1]],
+        #    ]
         ):
-    '''
+    ''' generator function to yield intersections between nodes and a vectorized track segments '''
+
 
     filters = [
-            #lambda track, rng: delta_knots(track, rng) < 50,
+            lambda track, rng: delta_knots(track, rng) < 50,
             lambda track, rng: delta_meters(track, rng) < 10000,
+            lambda track, rng: delta_seconds(track, rng) > 0,
         ]
 
     '''
-
-    print(f'{track_merged["mmsi"]}\tcount={len(track_merged["time"])}')
-
-    '''
-    bounds_lon, bounds_lat = zones['hull_xy'][::2], zones['hull_xy'][1::2]
+    bounds_lon, bounds_lat = domain['hull_xy'][::2], domain['hull_xy'][1::2]
     west, east = np.min(bounds_lon), np.max(bounds_lon)
     south, north = np.min(bounds_lat), np.max(bounds_lat)
 
@@ -184,28 +171,16 @@ def geofence(track_merged, zones, dbpath, colnames,
     transit_edges = []
 
     for rng in segment(track_merged, maxdelta=maxdelta, minsize=1):
+        # apply filters
         mask = filtermask(track_merged, rng, filters, first_val=True)
-        if sum(mask) == 0: 
-            print(f'{track_merged["mmsi"]} mmsi filtered, skipping rng = {rng}')
-            continue
-
         n = sum(mask)
-        if n <= 1: continue
-        subset = np.array(rng)[mask]  #[c:nc]
+        if n <= 1: 
+            print(f'\tskipping rng = {rng}')
+            continue
+        subset = np.array(rng)[mask] 
 
-        # get subset of zones that intersect with track
-        in_zones = {}
-        for zonerng in range(0, len(subset), 1000):
-            if not len(track_merged['lon'][subset][zonerng:zonerng+1000]) > 1:
-                print('warning: skipping track segment of length 1')
-                continue
-            in_zones.update({ k:v for k,v in zones['geoms'].items() if LineString(zip(track_merged['lon'][subset][zonerng:zonerng+1000], track_merged['lat'][subset][zonerng:zonerng+1000])).intersects(v) })
-
-        # from these zones, get zone for individual points
-        zoneID = np.array(
-                list(([k for k,v in in_zones.items() if v.contains(p)] or ['Z0'])[0] 
-                    for p in map(Point, zip(track_merged['lon'][subset], track_merged['lat'][subset]))),
-                dtype=object)
+        zoneID = np.array([domain.point_in_polygon(x, y) for x, y in zip(track_merged['lon'][subset], track_merged['lat'][subset])], dtype=object)
+        in_zones = set(zoneID)
 
         # append zone context to track rows
         transit_window = np.vstack((
@@ -215,57 +190,70 @@ def geofence(track_merged, zones, dbpath, colnames,
                 *(track_merged[col][subset] for col in colnames if col in track_merged['dynamic']),
                 np.append(delta_knots(track_merged, subset), [0]),
                 zoneID,
-                np.array([zones['domain'] for _ in range(n)]),
+                np.array([domain.name for _ in range(n)]),
             )).T
 
         transits.append(transit_window)
-        transit_nodes.append(set(in_zones.keys()))
+        transit_nodes.append(in_zones)
         transit_edges.append(zoneID)
 
     # concatenate non-transiting segments 
-    i = 0
-    while i < len(transit_nodes)-1:
-        if (len(transit_nodes[i]) == 1 and transit_edges[i][-1] == transit_edges[i+1][0]):
-            transits[i] = np.vstack((transits[i], transits.pop(i+1)))
-            transit_nodes.pop(i)
-            transit_edges[i] = np.append(transit_edges[i], transit_edges.pop(i+1))
+    while len(transit_nodes) > 1:
+        if ( len(transit_nodes[0]) <= 1 and (
+                (len(transit_nodes[0]) == len(transit_nodes[1]) and transit_edges[0][-1] == transit_edges[1][0]) or 
+                (set(['Z0', 'NULL']).issuperset(transit_nodes[1])))):
+            transits[0] = np.vstack((transits[0], transits.pop(1)))
+            transit_nodes.pop(0)
+            transit_edges[0] = np.append(transit_edges[0], transit_edges.pop(1))
         else:
-            i += 1
+            yield transits.pop(0), transit_nodes.pop(0), transit_edges.pop(0)
+            #i += 1
+    if len(transit_nodes) > 0:
+        yield transits.pop(0), transit_nodes.pop(0), transit_edges.pop(0)
+    else:
+        print('empty transit nodes')
+    assert len(transits) == 0, f'len(transits) = {len(transits)}'
+    assert len(transit_nodes) == 0, f'len(transit_nodes) = {len(transit_nodes)}'
+    assert len(transit_edges) == 0, f'len(transit_edges) = {len(transit_edges)}'
 
-    return transits, transit_nodes, transit_edges
+    #return transits, transit_nodes, transit_edges
 
 
-def agg_transit_windows(track_merged, zones, dbpath, colnames, *args):
-    ''' parallel process function for segmenting and geofencing tracks
-        
-        yields merged sets of rows with regions context 
-    '''
+def agg_transit_windows(track_merged, domain, dbpath, colnames, **kwargs):
+    ''' parallel process function '''
+    #transits, transit_nodes, transit_edges = geofence(track_merged, domain, dbpath, colnames, **kwargs)
+    print(f'{track_merged["mmsi"]}\tcount={len(track_merged["time"])}')
+
     statrows = []
-    transits, transit_nodes, transit_edges = geofence(track_merged, zones, dbpath, colnames, *args)
-
     # collect transits between zone boundaries
-    for transit_window, zoneID in zip(transits, transit_edges):
-        setrng = np.array(range(len(track_merged['time'])))
-        intersection = np.array(list(set(np.append(np.append([0], np.where(transit_window[:-1,-2] != transit_window[1:,-2])[0] +1), [len(transit_window)-1]))), dtype=object)
-
-        track_stats = segmentinfo(track_merged, transit_window, src_zone=zoneID[intersection[-2]], domain=zones['domain'])
-
-        # aggregate zone stats at transit nodes using sliding windows
-        for zoneidx, nextzoneidx, thirdzoneidx in zip(intersection[:-2], intersection[1:-1], intersection[2:]):
-            zoneset = setrng[zoneidx : nextzoneidx]
-            assert not len(zoneset) == 0
-            nextzoneset = setrng[nextzoneidx : thirdzoneidx]
-            
-            src_zone = zoneID[zoneidx]
-            track_stats = segmentinfo(track_merged, transit_window, src_zone=src_zone, domain=zones['domain'])
+    for transit_window, in_zones, zoneID in geofence(track_merged, domain, dbpath, colnames, **kwargs):
+        #setrng = np.array(range(len(track_merged['time'])))
+        setrng = np.array(range(len(transit_window)))
+        intersection = np.append(np.append([0], np.where(transit_window[:-1,-2] != transit_window[1:,-2])[0] +1), [len(transit_window)-1])
+        if intersection[-2] == intersection[-1]: intersection = intersection[:-1]
+        if len(intersection) > 2: 
+            #intersection = np.append([0], intersection)
+            # aggregate zone stats at transit nodes using sliding windows
+            for zoneidx, nextzoneidx, thirdzoneidx in zip(intersection[:-2], intersection[1:-1], intersection[2:]):
+                zoneset = setrng[zoneidx : nextzoneidx]
+                if len(zoneset) == 1: zoneset = np.append(zoneset, setrng[zoneidx: nextzoneidx])
+                nextzoneset = setrng[nextzoneidx : thirdzoneidx]
+                if len(nextzoneset) == 1: nextzoneset = np.append(nextzoneset, setrng[nextzoneidx : thirdzoneidx])
+                
+                src_zone = zoneID[zoneidx]
+                track_stats = segmentinfo(track_merged, transit_window, src_zone=src_zone, domainname=domain.name)
+                track_stats['src_stats'] = zone_stats(track_merged, zoneset)
+                track_stats['rcv_zone']  = zoneID[nextzoneidx]
+                track_stats['rcv_stats'] = zone_stats(track_merged, nextzoneset)
+                track_stats['transit_nodes'] = f"{track_stats['src_zone']}_{track_stats['rcv_zone']}"
+                statrows.append(track_stats)
+        else:
+            if in_zones == {'Z0'}: continue 
+            zoneset = setrng[intersection[-2]:]
+            track_stats = segmentinfo(track_merged, transit_window, src_zone=zoneID[intersection[-2]], domainname=domain.name)
             track_stats['src_stats'] = zone_stats(track_merged, zoneset)
-            track_stats['rcv_zone']  = zoneID[nextzoneidx]
-            track_stats['rcv_stats'] = zone_stats(track_merged, nextzoneset)
-            statrows.append(track_stats.copy())
-
-        zoneset = setrng[intersection[-2]:]
-        track_stats['src_stats'] = zone_stats(track_merged, zoneset)
-        statrows.append(track_stats)
+            track_stats['transit_nodes'] = f"{track_stats['src_zone']}"
+            statrows.append(track_stats)
 
     if len(statrows) == 0: 
         return 
@@ -278,9 +266,7 @@ def agg_transit_windows(track_merged, zones, dbpath, colnames, *args):
     return 
 
 
-def graph(merged, zones, dbpath, parallel=0, *args):
-    #merged = merge_layers(rows, zones, dbpath)
-
+def graph(merged, domain, dbpath, parallel=0, **kwargs):
     colnames = [
         'mmsi', 'time', 'lon', 'lat', 
         'cog', 'sog', 'msgtype',
@@ -294,12 +280,13 @@ def graph(merged, zones, dbpath, parallel=0, *args):
 
     if not parallel: 
         for mmsiset in merged:
-            agg_transit_windows(next(trackgen(mmsiset, colnames=colnames)), zones, dbpath, colnames, *args)
+            agg_transit_windows(next(trackgen(mmsiset, colnames=colnames)), domain, dbpath, colnames, **kwargs)
     else:
         with Pool(processes=parallel) as p:
-            fcn = partial(agg_transit_windows, zones=zones, dbpath=dbpath, colnames=colnames, *args)
+            fcn = partial(agg_transit_windows, domain=domain, dbpath=dbpath, colnames=colnames, **kwargs)
             # map track generator to anonymous fcn for each process in processing pool
             p.imap_unordered(fcn, (next(trackgen(m, colnames=colnames)) for m in merged), chunksize=1)
+            #p.map(fcn, (next(trackgen(m, colnames=colnames)) for m in merged), chunksize=1)
             p.close()
             p.join()
 
