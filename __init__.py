db67470e684d5bead50c63c3a446db59d2b44a11
matt24smith <matt24smith@gmail.com>
2021-08-31 16:36:06 -0300

    update config parsing

--- __init__.py
+++ __init__.py
@@ -1,19 +1,35 @@
 import os
+import shlex
 
 pkgname = 'ais'
 cfgfile = os.path.join(os.path.expanduser('~'), '.config', f'{pkgname}.cfg')
 
 if os.path.isfile(cfgfile):
-    exec(open(cfgfile).read())
+
+    with open(cfgfile) as f:
+        cfgs = shlex.shlex(instream=f, posix=False, punctuation_chars=True)
+        cfgs.whitespace = '\t\r\n'
+        cfgs.whitespace_split=True
+        cmd = True
+
+        while cmd != '':
+            cmd = cfgs.read_token()
+            exec(cmd)
+
 else:
     default = f'''\n
-    import os
     dbpath = os.path.join(os.path.expanduser('~'), f'{pkgname}.db')
     data_dir = os.path.join(os.path.expanduser('~'), f'{pkgname}') + os.path.sep
     zones_dir = os.path.join(data_dir, 'zones') + os.path.sep '''
 
-    print(f'no config file found at {cfgfile}. applying default configs: {default}')
+    printdefault = lambda s: '\n'.join(map(
+        lambda l: l.split('=')[0] + '= "' + eval(l.split('=')[1]) + '"', 
+        s.replace('\n\n','').split('\n')
+    ))
+
+    print(f'no config file found at {cfgfile}. applying default configs:\n\n{printdefault(default)}')
     exec(default)
+
 #import .track_geom
 
 #import .database
