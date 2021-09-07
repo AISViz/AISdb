eaf64a3a1a3d4ef490147e8bc52d195a348b8a0f
matt s <matt24smith@gmail.com>
2021-09-07 14:46:32 -0300

    update config parsing

--- ais/__init__.py
+++ ais/__init__.py
@@ -1,6 +1,7 @@
 import os
 import sys
-import shlex
+#import shlex
+import configparser
 
 
 sys.path.append(os.path.dirname(__file__))
@@ -8,35 +9,34 @@ pkgname = 'ais'
 cfgfile = os.path.join(os.path.expanduser('~'), '.config', f'{pkgname}.cfg')
 
 
-if os.path.isfile(cfgfile):
 
-    with open(cfgfile) as f:
-        cfgs = shlex.shlex(instream=f, posix=False, punctuation_chars=True)
-        cfgs.whitespace = '\t\r\n'
-        cfgs.whitespace_split=True
-        cmd = True
-
-        while cmd != '':
-            cmd = cfgs.read_token()
-            exec(cmd)
-
-else:
-
-    default = f'''\n
 dbpath = os.path.join(os.path.expanduser('~'), f'{pkgname}.db')
 data_dir = os.path.join(os.path.expanduser('~'), f'{pkgname}') + os.path.sep
 tmp_dir = os.path.join(data_dir, 'tmp_parsing') + os.path.sep 
 zones_dir = os.path.join(data_dir, 'zones') + os.path.sep 
-'''
+rawdata_dir = os.path.join(data_dir, 'rawdata') + os.path.sep
 
-    printdefault = lambda s: '\n'.join(map(
-        lambda l: l.split('=')[0] + '= "' + eval(l.split('=')[1]) + '"', 
-        s.replace('\n\n','').split('\n')
-    ))
+if os.path.isfile(cfgfile):
+    cfg = configparser.ConfigParser()
+    with open(cfgfile, 'r') as f:
+        cfg.read_string('[DEFAULT]\n' + f.read())
 
-    exec(default)
-    print(f'''# no config file found, applying default configs:\n\n{printdefault(default)}\n
-# to remove this warning, copy and paste this text to {cfgfile}''')
+    settings = dict(cfg['DEFAULT'])
+
+    dbpath = settings['dbpath']             if 'dbpath'      in settings.keys() else dbpath
+    data_dir = settings['data_dir']         if 'data_dir'    in settings.keys() else data_dir
+    tmp_dir = settings['tmp_dir']           if 'tmp_dir'     in settings.keys() else tmp_dir
+    zones_dir = settings['zones_dir']       if 'zones_dir'   in settings.keys() else zones_dir
+    rawdata_dir = settings['rawdata_dir']   if 'rawdata_dir' in settings.keys() else zones_dir
+
+else:
+
+    printdefault = lambda names, vals: '\n'.join([f'{n} = {v}' for n, v in zip(names, vals)])
+
+    print(f'''no config file found, applying default configs:\n\n{
+    printdefault(names=['dbpath', 'data_dir', 'tmp_dir', 'zones_dir',  'rawdata_dir'], 
+                 vals=[dbpath, data_dir, tmp_dir, zones_dir,  rawdata_dir])
+    }\n\nto remove this warning, copy and paste the above text to {cfgfile} ''')
 
 #import .track_geom
 
