7989f1d2de384248b972c2ef6bbcfc59cfb19eb8
matt24smith <matt24smith@gmail.com>
2021-12-20 04:03:21 -0400

    rename docker folder

---
+++ docker/entry.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import socket
+
+
+addr = socket.gethostbyaddr(socket.gethostname())[2][0]
+
+print("Starting environment over SSH\n")
+print(f"ssh -X ais_env@{addr} -i ~/.ssh/id_aisdb", end="\n\n", flush=True)
+
+cmd = f'''
+/sbin/sshd -D -e 
+-h /run/secrets/host_ssh_key 
+-oAuthorizedKeysFile=/run/secrets/host_authorized_keys 
+-oDenyUsers=root 
+-oKbdInteractiveAuthentication=no 
+-oPasswordAuthentication=no 
+-oPermitEmptyPasswords=no 
+-oPrintMotd=no 
+-oPort=22 
+-oPubkeyAuthentication=yes 
+-oUseDNS=no 
+-oX11Forwarding=yes 
+-oX11UseLocalhost=no 
+{' '.join(sys.argv[1:])}'''.replace('\n', ' ')
+
+os.system(cmd)
+
