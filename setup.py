882c6b9dc1ba915d1e5383d761446f65f3d47779
matt s <matt24smith@gmail.com>
2021-12-26 14:23:09 -0400

    formatting

--- setup.py
+++ setup.py
@@ -14,36 +14,36 @@ if majorver < 21:
 
 
 setup(name='aisdb',
-      version='0.1',
-      description='AIS Database and Processing Utils',
-      author='Matt Smith',
-      author_email='matthew.smith@dal.ca',
-      url='https://gitlab.meridian.cs.dal.ca/matt_s/ais_public',
-      license='GNU General Public License v3.0',
-      python_requires='>=3.8',
-      packages=[
-          'aisdb', 'aisdb.database', 'aisdb.webdata', 
-        ],
-      setup_requires=[
-          'numpy',
-          'pip>=21.1.0',
-          'wheel',
-        ],
-      install_requires=[
-          'numpy',
-          'packaging', 
-          'pillow',
-          'pip>=21.1.0',
-          'pyais', 
-          'requests', 
-          'selenium', 
-          'shapely', 
-          'tqdm',
-        ],
-      extras_require={
-          'testing' : ['pytest',],
-          'monitor' : ['pytest', 'pytest-monitor',], 
-          'docs'    : ['sphinx',],
-        },
-     )
+        version='0.1',
+        description='AIS Database and Processing Utils',
+        author='Matt Smith',
+        author_email='matthew.smith@dal.ca',
+        url='https://gitlab.meridian.cs.dal.ca/matt_s/ais_public',
+        license='GNU General Public License v3.0',
+        python_requires='>=3.8',
+        packages=[
+            'aisdb', 'aisdb.database', 'aisdb.webdata',
+            ],
+        setup_requires=[
+            'numpy',
+            'pip>=21.1.0',
+            'wheel',
+            ],
+        install_requires=[
+            'numpy',
+            'packaging',
+            'pillow',
+            'pip>=21.1.0',
+            'pyais>=1.6.1',
+            'requests',
+            'selenium',
+            'shapely',
+            'tqdm',
+            ],
+        extras_require={
+            'testing' : ['pytest',],
+            'monitor' : ['pytest', 'pytest-monitor',],
+            'docs'    : ['sphinx',],
+            },
+        )
 
