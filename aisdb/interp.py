ed0ffaf7c51060df44b192f674d217222dae9000
matt24smith <matt24smith@gmail.com>
2022-03-16 17:57:01 -0300

    refactor track interpolation

--- aisdb/interp.py
+++ aisdb/interp.py
@@ -4,68 +4,54 @@ from datetime import timedelta
 
 import numpy as np
 
-from gis import dt_2_epoch
-from proc_util import _segment_rng
+
+def np_interp_linear(track, key, intervals):
+    return np.interp(
+        x=intervals.astype(int),
+        xp=track['time'].astype(int),
+        fp=track[key].astype(float),
+        left=np.nan,
+        right=np.nan,
+        period=None,
+    )
 
 
-def interp_time(tracks,
-                start,
-                stop,
-                step=timedelta(minutes=10),
-                maxdelta=timedelta(hours=3),
-                minsize=3):
-    ''' segment and interpolate tracks to 10 minute intervals
+def interp_time(tracks, step=timedelta(minutes=10)):
+    ''' linear interpolation on vessel trajectory
 
         args:
-            tracks:
-                dict of messages sorted by mmsi then time.
+            tracks (dict)
+                messages sorted by mmsi then time.
                 uses mmsi as key with columns: time lon lat cog sog name .. etc
-            start:
-                beginning of temporal range as datetime
-            stop:
-                end of temporal range as datetime
-            step:
-                temporal interpolation interval as timedelta
-            maxdelta:
-                timedelta threshold at which to partition track segments
+            step (datetime.timedelta)
+                interpolation interval
 
         returns:
             dictionary of interpolated tracks
-
-        debug:
-            cur.scroll(0, mode='absolute')
-            rows = cur.fetchall()
     '''
-    # timestamp  = np.arange(start, stop+step, step).astype(datetime)
-    timestamp = np.arange(dt_2_epoch(start), dt_2_epoch(stop + step),
-                          step.total_seconds())
-    # intervals  = np.array(list(map(int, map(datetime.timestamp, timestamp))))
-    intervals = np.array(list(map(int, timestamp)))
-    interpfcn = lambda track, segments, intervals=intervals: {
-        **{k: track[k]
-           for k in ('mmsi', 'name', 'type')},
-        'time':
-        timestamp,
-        'seg': [
-            {
-                n: np.interp(
-                    x=intervals,
-                    # xp=list(map(datetime.timestamp, track['time'][rng])),
-                    xp=track['time'][rng],
-                    fp=track[n][rng],
-                    left=np.nan,
-                    right=np.nan,
-                    period=None,
-                )
-                for n in ['lon', 'lat', 'cog', 'sog']
-            } for rng in segments
-        ],
-        'rng': [
-            range(
-                np.nonzero(timestamp >= track['time'][rng][0])[0][0],
-                np.nonzero(timestamp <= track['time'][rng][-1])[0][-1])
-            for rng in segments
-        ],
-    }
     for track in tracks:
-        yield interpfcn(track, list(_segment_rng(track, maxdelta, minsize)))
+
+        if track['time'].size <= 1:
+            yield track.copy()
+            continue
+
+        intervals = np.arange(
+            start=track['time'][0],
+            stop=track['time'][-1],
+            step=int(step.total_seconds()),
+        ).astype(int)
+
+        itr = dict(
+            **{k: track[k]
+               for k in track['static']},
+            time=intervals,
+            static=track['static'],
+            dynamic=track['dynamic'],
+            **{
+                k: np_interp_linear(track, k, intervals)
+                for k in track['dynamic'] if k != 'time'
+            },
+        )
+        yield itr.copy()
+
+    return
