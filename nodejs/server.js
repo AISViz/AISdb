168a615df9904f6f5deee71c0b66a05e0423160a
matt <matthew.smith@dal.ca>
2022-03-29 19:10:45 +0000

    switch to port 8080 for docs
    This commit consolidates 4 consecutive commits by matt.

--- nodejs/server.js
+++ nodejs/server.js
@@ -3,16 +3,16 @@ const { networkInterfaces } = require('os');
 
 const express = require('express');
 const app = express();
-const port = 8085;
+const port = 8080;
 
 
 app.use('/', express.static('../docs/html'));
 //app.use('/rust', express.static('../docs/html/rust'));
 //app.use('/rust/doc', express.static('../docs/html/rust/doc'));
-app.use('/rust/doc/aisdb_bin', express.static('../docs/html/rust/doc/aisdb_bin'));
-app.use('/rust/doc/aisdb_lib', express.static('../docs/html/rust/doc/aisdb_lib'));
-app.use('/rust/doc/aisdb_bin/index.html', express.static('../docs/html/rust/doc/aisdb_bin/index.html'));
-app.use('/rust/doc/aisdb_lib/index.html', express.static('../docs/html/rust/doc/aisdb_lib/index.html'));
+//app.use('/rust/doc/aisdb_bin', express.static('../docs/html/rust/doc/aisdb_bin'));
+//app.use('/rust/doc/aisdb_lib', express.static('../docs/html/rust/doc/aisdb_lib'));
+//app.use('/rust/doc/aisdb_bin/index.html', express.static('../docs/html/rust/doc/aisdb_bin/index.html'));
+//app.use('/rust/doc/aisdb_lib/index.html', express.static('../docs/html/rust/doc/aisdb_lib/index.html'));
 
 
 app.listen(port, '::', () => {
