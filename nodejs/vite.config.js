1d25f9083157ab08ebff720d1d968f61d20485d1
matt24smith <matt24smith@gmail.com>
2022-03-30 11:53:25 -0300

    remove build assets #noupdate

--- nodejs/vite.config.js
+++ nodejs/vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const { resolve } = require('path')
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+//export default defineConfig({
+//module.exports = ({
+//  plugins: [react()]
+//})
 
 module.exports = {
-  root: 'map'
+  plugins: [react()],
+  root: 'map',
+  //root: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        map: resolve(__dirname, 'map/'),
+      }
+    }
+  }
 }
