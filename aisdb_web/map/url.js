c549f11616ea26c40f26006adf2d2994844e3030
matt24smith <matt24smith@gmail.com>
2022-08-23 12:32:55 -0300

    #noupdate wait for page load before parsing URI

--- aisdb_web/map/url.js
+++ aisdb_web/map/url.js
@@ -31,6 +31,11 @@ function isNumeric(n) {
  * http://localhost:3000/?ecoregions=1&x=-65&y=59.75&z=4&start=2021-01-01&end=2021-01-02&xmin=-95.7&xmax=-39.5&ymin=34.4&ymax=74.2
  */
 async function parseUrl() {
+  while (mapview === null) {
+    await new Promise((resolve) => {
+      return setTimeout(resolve, 250);
+    });
+  }
   if (isNumeric(urlParams.get('x')) && isNumeric(urlParams.get('y'))) {
     let lon = parseFloat(urlParams.get('x'));
     let lat = parseFloat(urlParams.get('y'));
