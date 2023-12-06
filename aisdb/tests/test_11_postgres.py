c5d30d04e5788c0e3cb1e1f20a3e24640b8ebd0a
Jay-PC <Jay_Tharwani1992@yahoo.com>
2023-12-06 11:53:18 -0400

    updated test_01 and test_11 by putting postgres function into test_11_postgres.py

--- aisdb/tests/test_11_postgres.py
+++ aisdb/tests/test_11_postgres.py
@@ -23,3 +23,22 @@ with PostgresDBConn('postgresql://127.0.0.1:443/postgres?') as dbconn:
         res = cur.fetchall()
         print(res)
 '''
+
+
+def test_create_from_CSV_postgres(tmpdir):
+    testingdata_csv = os.path.join(os.path.dirname(__file__), 'testdata',
+                                   'test_data_20210701.csv')
+    with PostgresDBConn(**postgres_test_conn) as dbconn:
+        decode_msgs(
+            dbconn=dbconn,
+            filepaths=[testingdata_csv],
+            source='TESTING',
+            vacuum=False,
+        )
+        cur = dbconn.cursor()
+        cur.execute(
+            # need to specify datbabase name in SQL statement
+            "SELECT table_name FROM information_schema.tables "
+            "WHERE table_schema = 'public' ORDER BY table_name;")
+        tables = [row["table_name"] for row in cur.fetchall()]
+        assert 'ais_202107_dynamic' in tables
