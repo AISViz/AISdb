d4516897c24bfbe59db09c2dcfc8396ae9670a4d
Gabriel Spadon <gabriel@spadon.com.br>
2024-01-16 18:10:53 -0400

    Expanding the feature set based on all relevant information we have on the dataset.

--- aisdb/aisdb_sql/cte_dynamic_clusteredidx.sql
+++ aisdb/aisdb_sql/cte_dynamic_clusteredidx.sql
@@ -1,9 +1,12 @@
   SELECT 
     d.mmsi, 
-    d.time, 
+    d.time,
     d.longitude,
     d.latitude,
+    d.rot,
     d.sog,
-    d.cog
+    d.cog,
+    d.heading,
+    d.maneuver
   FROM ais_{}_dynamic AS d
   WHERE
