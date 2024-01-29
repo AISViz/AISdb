f43830e6a84ecbd606631acabab9a4094ca7f826
Jay-PC <Jay_Tharwani1992@yahoo.com>
2024-01-29 14:21:17 -0400

    feat: add geo_interp_time for geometric track interpolation
    Add geo_interp_time to aisdb.interp. It projects tracks to EPSG:3857,
    interpolates positions over fixed time steps, and derives course over
    ground from geodesic azimuths when transforming back. Extend
    test_06_interp.py to cover the new function.

--- aisdb/interp.py
+++ aisdb/interp.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 
 import numpy as np
 import warnings
-from pyproj import Transformer
+from pyproj import Transformer, Geod
 
 
 def np_interp_linear(track, key, intervals):
@@ -30,6 +30,7 @@ def interp_time(tracks, step=timedelta(minutes=10)):
         Example:
             >>> import numpy as np
             >>> from datetime import timedelta, datetime
+            >>> import aisdb
 
             >>> y1, x1 = -66.84683, -61.10595523571155
             >>> y2, x2 = -66.83036, -61.11595523571155
@@ -80,6 +81,93 @@ def interp_time(tracks, step=timedelta(minutes=10)):
 
     return
 
+
+
+def geo_interp_time(tracks, step=timedelta(minutes=10), original_crs = 4269):
+    ''' Geometric interpolation on vessel trajectory, assumes default EPSG:4269
+
+            args:
+                tracks (dict)
+                    messages sorted by mmsi then time.
+                    uses mmsi as key with columns: time lon lat cog sog name .. etc
+                step (datetime.timedelta)
+                    interpolation interval
+
+            returns:
+                dictionary of interpolated tracks
+
+            Example:
+                >>> import numpy as np
+                >>> from datetime import timedelta, datetime
+                >>> import aisdb
+
+                >>> y1, x1 = -66.84683, -61.10595523571155
+                >>> y2, x2 = -66.83036, -61.11595523571155
+                >>> y3, x3 = 48.2815186388735, -61.12595523571155
+                >>> t1 = dt_2_epoch( datetime(2021, 1, 1, 1) )
+                >>> t2 = dt_2_epoch( datetime(2021, 1, 1, 2) )
+                >>> t3 = dt_2_epoch(datetime(2021, 1, 1, 3))
+
+                >>> # creating a sample track
+                >>> tracks_short = [
+                ...                 dict( lon=np.array([x1, x2, x3]),
+                ...                 lat=np.array([y1, y2, y3]),
+                ...                 time=np.array([t1, t2, t3]),
+                ...                 dynamic=set(['lon', 'lat', 'time']),
+                ...                 static = set() ) ]
+
+                >>> tracks__ = aisdb.interp.geo_interp_time(tracks_short, timedelta(minutes=10))
+                >>> for tr in tracks__:
+                ...     print(tr)
+
+        '''
+    new_crs = 3857
+    fwd_trans = Transformer.from_crs(original_crs, new_crs, always_xy=True)
+    back_trans = Transformer.from_crs(new_crs,original_crs, always_xy=True)
+
+    for track in tracks:
+        if track['time'].size <= 1:
+            # yield track
+            warnings.warn('cannot interpolate track of length 1, skipping...')
+            continue
+
+        intervals = np.arange(
+            start=track['time'][0],
+            stop=track['time'][-1] + int(step.total_seconds()),
+            step=int(step.total_seconds()),
+        ).astype(int)
+
+        assert len(intervals) >= 1
+
+        itr = dict(
+            **{k: track[k]
+               for k in track['static']},
+            time=intervals,
+            static=track['static'],
+            dynamic=track['dynamic']
+        )
+        if 'lat' in track['dynamic']:
+            x, y = fwd_trans.transform(track['lon'], track['lat'])
+            x = np.interp(x=intervals.astype(int),
+                      xp=track['time'].astype(int),
+                      fp=x.astype(float))
+            y = np.interp(x=intervals.astype(int),
+                          xp=track['time'].astype(int),
+                          fp=y.astype(float))
+            itr['lon'], itr['lat'] = back_trans.transform(x,y)
+            if 'cog' in track['dynamic']:
+                geod = Geod(ellps="WGS84")
+                courses, _, _ = geod.inv(itr['lon'][:-1], itr['lat'][:-1], itr['lon'][1:], itr['lat'][1:])
+                itr['cog'] = np.append(courses, track['cog'][-1])
+        for key in track['dynamic']:
+            if key not in itr:
+                itr[key] = np.interp(x=intervals.astype(int),
+                     xp=track['time'].astype(int),
+                     fp=track[key].astype(float))
+
+        yield itr
+
+    return
 
 def interp_spacing(spacing : int, tracks, crs = 4269):
     '''linear interpolation on vessel trajectory
