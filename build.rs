5b1cf2df22f819d8b272b3fcb19ed4eec42688f6
Jinkun Chen <i@jinkunchen.com>
2024-02-24 15:37:41 -0400

    Integration with AISdb-Web Directory

--- build.rs
+++ build.rs
@@ -1,31 +1,58 @@
-#[cfg(not(debug_assertions))]
 use std::fs::{remove_file, File};
-#[cfg(not(debug_assertions))]
 use std::io::Write;
 use std::path::PathBuf;
 use std::process::Command;
 
-#[cfg(not(debug_assertions))]
 use reqwest::blocking::get;
 use wasm_opt::OptimizationOptions;
 
-#[cfg(not(debug_assertions))]
-fn download_gitlab_artifacts(branch: &str) -> Result<PathBuf, String> {
-    let url = format!(
-        "https://git-dev.cs.dal.ca/api/v4/projects/132/jobs/artifacts/{}/download?job=wasm-assets",
-        branch
-    );
+#[derive(PartialEq)]
+enum DownloadType {
+    SourceCode,
+    Build,
+}
+
+fn check_source_code_folder() -> bool {
+    let path = PathBuf::from("AISdb-Web-main");
+    path.exists()
+}
+
+fn remove_source_code_folder() {
+    let path = PathBuf::from("AISdb-Web-main");
+    if path.exists() {
+        std::fs::remove_dir_all(path).expect("removing source code folder");
+    }
+}
+
+fn download_source(version: &str, download_type: &DownloadType) -> Result<PathBuf, String> {
+    let (url, type_of_download, file_name) = match download_type {
+        DownloadType::SourceCode => ("https://github.com/AISViz/aisdb-web/archive/refs/heads/main.zip".to_string(),
+            "Source Code", "main.zip"),
+        DownloadType::Build => (format!("https://github.com/AISViz/AISdb-Web/releases/{}/download/aisdb_web.zip", version),
+            "Build Project", "aisdb_web.zip"),
+    };
+
     let zipfile_bytes = get(url)
-        .expect("downloading web asset artifacts")
+        .expect(format!("downloading {}", type_of_download).as_str())
         .bytes()
         .expect("get asset bytes");
+    
     //assert!(zipfile_bytes.len() > 64); // make sure we didnt get error 404
     if zipfile_bytes.len() <= 64 {
-        eprintln!("branch:{}, result: {:#?}", branch, zipfile_bytes);
-        return Err("assert!(zipfile_bytes.len() > 64)".to_string()); // make sure we didnt get error 404
+        eprintln!("Download: {}, result: {:#?}", type_of_download, zipfile_bytes);
+        let (what_to_download, extracted_folder, url) = match download_type {
+            DownloadType::SourceCode => {
+                ("source code", "AISdb-Web-main", "https://github.com/AISViz/AISdb-Web/")
+            },
+            DownloadType::Build => {
+                ("source code", "aisdb_web", "https://github.com/AISViz/AISdb-Web/releases/")
+            }
+        };
+        eprintln!("Please manually download the {} from the following link, and put the extracted folder {} in the root of the project:\n{}", what_to_download, extracted_folder, url);
+        return Err("assert!(zipfile_bytes.len() > 64)".to_string()); // make sure we didnt get error 404s
     }
 
-    let zipfilepath = PathBuf::from("artifacts.zip");
+    let zipfilepath = PathBuf::from(file_name);
     let mut zipfile = File::create(&zipfilepath).expect("creating empty zipfile");
     zipfile
         .write_all(&zipfile_bytes)
@@ -43,34 +70,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     //println!("cargo:rerun-if-changed=./client_webassembly/src/*");
 
     // only do this for release builds
-    #[cfg(not(debug_assertions))]
+    // #[cfg(not(debug_assertions))]
     // download web assets from gitlab CD artifacts
     // if OFFLINE_BUILD is not set, it is expected that artifacts will be passed from previous job
-    if std::env::var("OFFLINE_BUILD").is_err() {
-        //let branch = "master";
-        let mut branch = String::from_utf8(
-            Command::new("git")
-                .args(["rev-parse", "--abbrev-ref", "HEAD"])
-                .output()
-                .expect("getting current git branch")
-                .stdout,
-        )
-        .unwrap();
-        if branch.is_empty() {
-            branch = "master".to_string();
-        }
 
-        // do download
-        let mut zipfilepath = download_gitlab_artifacts(&branch);
-        if let Err(_e) = zipfilepath {
-            eprintln!(
-                "warning: no artifacts found on {}, falling back to master branch...",
-                branch
-            );
-            zipfilepath = download_gitlab_artifacts("master");
-        }
-        let zipfilepath = zipfilepath?;
+    std::env::set_var("OFFLINE_BUILD", "1");
 
+    let (download_type, file_name, build_type) = match std::env::var("OFFLINE_BUILD") {
+        Ok(_) => (DownloadType::SourceCode, "main.zip", "OFFLINE_BUILD"),
+        Err(_) => (DownloadType::Build, "aisdb_web.zip", "ONLINE_BUILD"),
+    };
+
+    let version = "latest";
+
+    if (build_type == "OFFLINE_BUILD" && !check_source_code_folder()) || build_type == "ONLINE_BUILD"{
+        let zipfilepath = download_source(&version, &download_type)?;
         // unzip web assets into project
         let unzip = Command::new("unzip")
             .arg("-o")
@@ -81,14 +95,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         assert!(unzip.status.code().unwrap() == 0);
 
         // remove zipfile
-        remove_file("artifacts.zip").expect("deleting zip");
+        remove_file(file_name).expect("deleting zip");
     }
 
+
     // web assets may also be built locally if OFFLINE_BUILD is set
-    if std::env::var("OFFLINE_BUILD").is_ok() {
+    if build_type == "OFFLINE_BUILD" {
         // build wasm
         let wasm_build = Command::new("wasm-pack")
-            .current_dir("./client_webassembly/")
+            .current_dir("./AISdb-Web-main/web_assembly")
             .args([
                   "build",
                   "--target=web",
@@ -173,6 +188,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         eprintln!("{}", String::from_utf8_lossy(&vite_build_2.stderr[..]));
         assert!(vite_build_2.status.code().unwrap() == 0);
     }
+
+    remove_source_code_folder();
 
     assert!(PathBuf::from("./aisdb_web/map/pkg").exists());
 
