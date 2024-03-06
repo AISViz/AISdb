5d9d0049cca4555508167a4cd1c9cc7022e3a111
Gabriel Spadon <gabriel@spadon.com.br>
2024-03-06 18:01:16 -0400

    Update bathymetry.py for improved file handling and readability
    This update refactors code in bathymetry.py for better efficiency and readability. The changes include improved handling of raster files with a more convenient way to download and extract archives. The code is simplified in some areas for better readability as well. Comments throughout the code are updated for clarity, and unnecessary import statements and variables are removed. The filename extension and download URL have also been corrected. Relevant testing workflows in CI.yml have been uncommented to embrace these changes.

--- aisdb/webdata/bathymetry.py
+++ aisdb/webdata/bathymetry.py
@@ -1,52 +1,39 @@
-''' load bathymetry data from GEBCO raster files '''
+""" load bathymetric data from GEBCO raster files """
 
-import hashlib
 import os
 import warnings
-import zipfile
 from functools import reduce
 
 import numpy as np
+import py7zr
 import requests
 from tqdm import tqdm
 
 from aisdb.gis import shiftcoord
 from aisdb.webdata.load_raster import RasterFile
 
-url = 'https://www.bodc.ac.uk/data/open_download/gebco/gebco_2022/geotiff/'
+url = "http://bigdata5.research.cs.dal.ca/raster-bathy.7z"
 
 
 def _filebounds(fpath):
-    return {
-        f[0]: float(f[1:])
-        for f in fpath.split('gebco_2022_', 1)[1].rsplit('.tif', 1)[0].split(
-            '_')
-    }
+    return {f[0]: float(f[1:]) for f in fpath.split('gebco_2022_', 1)[1].rsplit('.tif', 1)[0].split('_')}
 
 
 class Gebco():
 
     def __init__(self, data_dir):
-        '''
+        """
             args:
                 data_dir (string)
-                    folder where rasters should be stored
-        '''
+                    folder where raters should be stored
+        """
         self.data_dir = data_dir
         assert os.path.isdir(data_dir)
         self.__enter__()
 
     def __enter__(self):
-        self.rasterfiles = {
-            f: _filebounds(f)
-            for f in {
-                k: None
-                for k in sorted([
-                    f for f in os.listdir(self.data_dir)
-                    if f[-4:] == '.tif' and 'gebco_2022' in f
-                ])
-            }
-        }
+        self.rasterfiles = {f: _filebounds(f) for f in
+            {k: None for k in sorted([f for f in os.listdir(self.data_dir) if f[-4:] == '.tif' and 'gebco_2022' in f])}}
 
         # download bathymetry rasters if missing
         self.fetch_bathymetry_grid()
@@ -59,46 +46,34 @@ class Gebco():
     def fetch_bathymetry_grid(self):  # pragma: no cover
         """ download geotiff zip archive and extract it """
 
-        zipf = os.path.join(self.data_dir, "gebco_2022_geotiff.zip")
+        zipf = os.path.join(self.data_dir, "raster-bathy.7z")
         try:
-
-            # download the file if necessary
             if not os.path.isfile(zipf):
-                print('downloading gebco bathymetry...')
+                print('Downloading bathymetric data...')
                 with requests.get(url, stream=True) as payload:
                     assert payload.status_code == 200, 'error fetching file'
                     with open(zipf, 'wb') as f:
-                        with tqdm(total=4011413504,
-                                  desc=zipf,
-                                  unit='B',
-                                  unit_scale=True) as t:
+                        with tqdm(total=4011413504, desc=zipf, unit='B', unit_scale=True) as t:
                             for chunk in payload.iter_content(chunk_size=8192):
                                 _ = t.update(f.write(chunk))
 
-                with open(zipf, 'rb') as z:
-                    sha256sum = hashlib.sha256(z.read()).hexdigest()
-                print('verifying checksum...')
-                assert sha256sum == '5ade15083909fcd6003409df678bdc6537b8691df996f8d806b48de962470cc3', \
-                    'checksum failed!'
-                with zipfile.ZipFile(zipf, 'r') as zip_ref:
+                with py7zr.SevenZipFile(zipf, mode='r') as zip_ref:
                     members = list(
-                        set(zip_ref.namelist()) -
-                        set(sorted(os.listdir(self.data_dir))))
-                    print('extracting bathymetry data...')
-                    zip_ref.extractall(path=self.data_dir, members=members)
+                        fpath for fpath in set(zip_ref.getnames()) - set(
+                            sorted(os.listdir(self.data_dir))) if '.tif' in fpath)
+                    print('Extracting bathymetric data...')
+                    zip_ref.extract(targets=members, path=self.data_dir)
         except (Exception, KeyboardInterrupt) as err:
-            os.remove(os.path.join(self.data_dir, "gebco_2022_geotiff.zip"))
-            raise (err)
+            os.remove(os.path.join(self.data_dir, "raster-bathy.7z"))
+            raise err
         return
 
     def _load_raster(self, key):
-        self.rasterfiles[key]['raster'] = RasterFile(
-            imgpath=os.path.join(self.data_dir, key))
+        self.rasterfiles[key]['raster'] = RasterFile(imgpath=os.path.join(self.data_dir, key))
 
     def _check_in_bounds(self, track):
         for lon, lat in zip(track['lon'], track['lat']):
-            if not (-180 <= lon <= 180) or not (-90 <= lat <=
-                                                90):  # pragma: no cover
+            if not (-180 <= lon <= 180) or not (-90 <= lat <= 90):  # pragma: no cover
                 warnings.warn('coordinates out of range!')
                 lon = shiftcoord([lon])[0]
                 lat = shiftcoord([lat], rng=90)[0]
@@ -106,8 +81,7 @@ class Gebco():
             if os.environ.get('DEBUG'):
                 tracer = False
             for key, bounds in self.rasterfiles.items():
-                if (bounds['w'] <= lon <= bounds['e']
-                        and bounds['s'] <= lat <= bounds['n']):
+                if bounds['w'] <= lon <= bounds['e'] and bounds['s'] <= lat <= bounds['n']:
                     tracer = True
                     if 'raster' not in bounds.keys():
                         self._load_raster(key)
@@ -124,27 +98,19 @@ class Gebco():
                 bounds['raster'].img.close()
 
     def merge_tracks(self, tracks):
-        ''' append `depth_metres` column to track dictionaries '''
+        """ append `depth_metres` column to track dictionaries """
         for track in tracks:
             # mapping of filepaths to the corresponding boundary region
-            raster_keys = np.array(list(self._check_in_bounds(track)),
-                                   dtype=object)
+            raster_keys = np.array(list(self._check_in_bounds(track)), dtype=object)
 
             # ensure that each vector time slice has a value
             if len(raster_keys) != len(track['time']):
-                raise ValueError('no rasters found for track')
-            bathy_segments = np.append(
-                np.append([0],
-                          np.where(raster_keys[:-1] != raster_keys[:1])[0]),
-                [len(raster_keys)],
-            )
+                raise ValueError('no raters found for track')
+            bathy_segments = np.append(np.append([0], np.where(raster_keys[:-1] != raster_keys[:1])[0]),
+                [len(raster_keys)], )
             track['depth_metres'] = reduce(np.append, [
-                self.rasterfiles[raster_keys[i]]
-                ['raster']._track_coordinate_values(
-                    track, rng=range(bathy_segments[i], bathy_segments[i + 1]))
-                for i in range(len(bathy_segments) - 1)
-            ]) * -1
+                self.rasterfiles[raster_keys[i]]['raster']._track_coordinate_values(track,
+                    rng=range(bathy_segments[i], bathy_segments[i + 1])) for i in range(len(bathy_segments) - 1)]) * -1
 
-            track['dynamic'] = set(track['dynamic']).union(
-                set(['depth_metres']))
+            track['dynamic'] = set(track['dynamic']).union({'depth_metres'})
             yield track
