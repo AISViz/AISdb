fcf64cc5321ea11cd4097a825c57db615377164f
Gabriel Spadon <gabriel@spadon.com.br>
2024-03-07 12:44:34 -0400

    Introduce dynamic and static triggers for PostgreSQL tables
    This commit creates new PostgreSQL trigger functions, namely `dynamic_insert` and `static_insert`, replacing the previous implementations inside `psql_createtable_static.sql` and `psql_createtable_dynamic_noindex.sql`. It also modifies the database decoder script to introduce these triggers in database tables for new rows insertion. Additionally, simplified tests in 'test_016_bathymetry.py' have been implemented for efficient test execution.

--- aisdb/aisdb_sql/psql_createtable_dynamic_noindex.sql
+++ aisdb/aisdb_sql/psql_createtable_dynamic_noindex.sql
@@ -1,25 +1,3 @@
-CREATE OR REPLACE FUNCTION dynamic_insert()
-RETURNS TRIGGER AS $$
-DECLARE
-    target_table TEXT;
-BEGIN
-    -- Assuming NEW.time is the epoch timestamp in seconds
-    target_table := lower('ais_' || TO_CHAR(TO_TIMESTAMP(NEW.time), 'YYYYMM') || '_dynamic');
-
-    BEGIN
-        -- Check if the target table exists, if not, create it
-        IF NOT EXISTS (SELECT 1 FROM pg_tables WHERE tablename = lower(target_table)) THEN
-            EXECUTE 'CREATE TABLE ' || target_table || ' (LIKE ' || TG_TABLE_NAME || ' INCLUDING ALL)';
-        END IF;
-        -- Insert the new row into the correct table
-        EXECUTE 'INSERT INTO ' || target_table || ' VALUES (($1).*)' USING NEW;
-    END;
-
-    -- Skip the original insert operation
-    RETURN NULL;
-END;
-$$ LANGUAGE plpgsql;
-
 CREATE TABLE IF NOT EXISTS ais_{0}_dynamic (
     mmsi INTEGER NOT NULL,
     time INTEGER NOT NULL,
