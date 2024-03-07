a70e80aabad53be2bfb9d900390a19083d13459b
Gabriel Spadon <gabriel@spadon.com.br>
2024-03-07 17:21:44 -0400

    Refactor code for improved readability
    Simplified the coding style for creating DBQuery instances across multiple test files to enhance readability and maintainability. This includes reducing argument segmentation and streamlining variable assignments. The refactoring also involves cleaning up unnecessary condition checks.

--- aisdb/tests/create_testing_data.py
+++ aisdb/tests/create_testing_data.py
@@ -4,10 +4,7 @@ import numpy as np
 from shapely.geometry import Polygon
 
 from aisdb import decode_msgs, DBConn
-from aisdb.database.create_tables import (
-    sql_createtable_dynamic,
-    sql_createtable_static,
-)
+from aisdb.database.create_tables import (sql_createtable_dynamic, sql_createtable_static, )
 from aisdb.gis import Domain
 
 
@@ -17,14 +14,11 @@ def sample_dynamictable_insertdata(*, dbconn):
     dbconn.execute(sql_createtable_static.format(month="200001"))
     dbconn.execute(sql_createtable_dynamic.format(month="200001"))
     dbconn.execute(
-        "INSERT OR IGNORE INTO ais_200001_dynamic (mmsi, time, longitude, latitude, cog, sog) VALUES (000000001, 946702800, -60.994833, 47.434647238127695, -1, -1)"
-    )
+        "INSERT OR IGNORE INTO ais_200001_dynamic (mmsi, time, longitude, latitude, cog, sog) VALUES (000000001, 946702800, -60.994833, 47.434647238127695, -1, -1)")
     dbconn.execute(
-        "INSERT OR IGNORE INTO ais_200001_dynamic (mmsi, time, longitude, latitude, cog, sog) VALUES (000000001, 946702820, -60.994833, 47.434647238127695, -1, -1)"
-    )
+        "INSERT OR IGNORE INTO ais_200001_dynamic (mmsi, time, longitude, latitude, cog, sog) VALUES (000000001, 946702820, -60.994833, 47.434647238127695, -1, -1)")
     dbconn.execute(
-        "INSERT OR IGNORE INTO ais_200001_dynamic (mmsi, time, longitude, latitude, cog, sog) VALUES (000000001, 946702840, -60.994833, 47.434647238127695, -1, -1)"
-    )
+        "INSERT OR IGNORE INTO ais_200001_dynamic (mmsi, time, longitude, latitude, cog, sog) VALUES (000000001, 946702840, -60.994833, 47.434647238127695, -1, -1)")
     dbconn.commit()
 
 
@@ -48,41 +42,27 @@ def sample_random_polygon(xscale=50, yscale=50):
 
 
 def sample_gulfstlawrence_bbox():
-    gulfstlawrence_bbox_xy = np.array([
-        (-71.64440346704974, 43.18445256159233),
-        (-71.2966623933639, 52.344721551389526),
-        (-51.2146153880073, 51.68484191466307),
-        (-50.345262703792734, 42.95158299927571),
-        (-71.64440346704974, 43.18445256159233),
-    ])
+    gulfstlawrence_bbox_xy = np.array([(-71.64440346704974, 43.18445256159233), (-71.2966623933639, 52.344721551389526),
+        (-51.2146153880073, 51.68484191466307), (-50.345262703792734, 42.95158299927571),
+        (-71.64440346704974, 43.18445256159233), ])
     return gulfstlawrence_bbox_xy.T
 
 
 def random_polygons_domain(count=10):
-    return Domain("testdomain",
-                  [{
-                      "name": f"random_{i:03}",
-                      "geometry": Polygon(zip(*sample_random_polygon()))
-                  } for i in range(count)])
+    return Domain("testdomain", [{"name": f"random_{i:03}", "geometry": Polygon(zip(*sample_random_polygon()))} for i in
+        range(count)])
 
 
 def sample_database_file(dbpath):
     """ test data for date 2021-11-01 """
     assert os.path.isdir(os.path.join(os.path.dirname(__file__), "testdata"))
-    datapath_csv = os.path.join(os.path.dirname(__file__), "testdata",
-                                "test_data_20210701.csv")
+    datapath_csv = os.path.join(os.path.dirname(__file__), "testdata", "test_data_20210701.csv")
     # no static data in nm4
-    datapath_nm4 = os.path.join(os.path.dirname(__file__), "testdata",
-                                "test_data_20211101.nm4")
+    datapath_nm4 = os.path.join(os.path.dirname(__file__), "testdata", "test_data_20211101.nm4")
     months = ["202107", "202111"]
     with DBConn(dbpath) as dbconn:
-        decode_msgs(
-            dbconn=dbconn,
-            filepaths=[datapath_csv, datapath_nm4],
-            source="TESTING",
-            vacuum=False,
-            skip_checksum=True,
-        )
+        decode_msgs(dbconn=dbconn, filepaths=[datapath_csv, datapath_nm4], source="TESTING", vacuum=False,
+            skip_checksum=True, )
         dbconn.aggregate_static_msgs(months)
         dbconn.commit()
     return months
--- aisdb/tests/test_010_network_graph.py
+++ aisdb/tests/test_010_network_graph.py
@@ -9,26 +9,13 @@ from aisdb.database.dbconn import DBConn
 from aisdb.database.dbqry import DBQuery
 from aisdb.gis import Domain
 from aisdb.network_graph import graph
-from aisdb.tests.create_testing_data import (
-    sample_database_file,
-    sample_gulfstlawrence_bbox,
-)
+from aisdb.tests.create_testing_data import (sample_database_file, sample_gulfstlawrence_bbox, )
 
-trafficDBpath = os.environ.get(
-    "AISDBMARINETRAFFIC",
-    os.path.join(
-        os.path.dirname(os.path.dirname(os.path.dirname(__file__))),
-        "testdata",
-        "marinetraffic_test.db",
-    ))
+trafficDBpath = os.environ.get("AISDBMARINETRAFFIC",
+    os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "testdata", "marinetraffic_test.db", ))
 
-data_dir = os.environ.get(
-    "AISDBDATADIR",
-    os.path.join(
-        os.path.dirname(os.path.dirname(os.path.dirname(__file__))),
-        "testdata",
-    ),
-)
+data_dir = os.environ.get("AISDBDATADIR",
+    os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "testdata", ), )
 
 lon, lat = sample_gulfstlawrence_bbox()
 z1 = Polygon(zip(lon, lat))
@@ -37,21 +24,8 @@ z3 = Polygon(zip(lon, lat - 45))
 
 
 def test_graph_minimal(tmpdir):
-    domain = Domain("gulf domain",
-                    zones=[
-                        {
-                            "name": "z1",
-                            "geometry": z1
-                        },
-                        {
-                            "name": "z2",
-                            "geometry": z2
-                        },
-                        {
-                            "name": "z3",
-                            "geometry": z3
-                        },
-                    ])
+    domain = Domain("gulf domain", zones=[{"name": "z1", "geometry": z1}, {"name": "z2", "geometry": z2},
+        {"name": "z3", "geometry": z3}, ])
 
     testdbpath = os.path.join(tmpdir, "test_graph_minimal.db")
 
@@ -60,16 +34,14 @@ def test_graph_minimal(tmpdir):
     end = start + timedelta(weeks=1)
 
     with DBConn(testdbpath) as ais_database:
-        qry = DBQuery(dbconn=ais_database, start=start, end=end,
-                      callback=sqlfcn_callbacks.in_bbox,
-                      fcn=sqlfcn.crawl_dynamic_static,
-                      **domain.boundary)
+        qry = DBQuery(dbconn=ais_database, start=start, end=end, callback=sqlfcn_callbacks.in_bbox,
+                      fcn=sqlfcn.crawl_dynamic_static, **domain.boundary)
 
         outputfile = os.path.join(tmpdir, "output.csv")
         print(f"raw count: {len(list(qry.gen_qry()))}")
 
-        graph(qry, outputfile=outputfile, data_dir=data_dir,
-              dbconn=ais_database, domain=domain, trafficDBpath=trafficDBpath)
+        graph(qry, outputfile=outputfile, data_dir=data_dir, dbconn=ais_database, domain=domain,
+              trafficDBpath=trafficDBpath)
     ais_database.close()
     if os.path.isfile(outputfile):
         os.remove(outputfile)
