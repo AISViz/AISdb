44ab2fea0a51f0fe11c5fe57eecfd3c8246fd7e1
Gabriel Spadon <gabriel@spadon.com.br>
2024-03-07 13:31:12 -0400

    Improve insert operations with dynamic SQL in triggers
    This update refines the insert operations within PostgreSQL trigger functions for both dynamic and static cases. The upgrade dynamically creates tables if they don't exist and then dynamically builds and executes insert statements. This method enhances efficiency and flexibility compared to previous static implementations.

--- aisdb/aisdb_sql/psql_static_trigger.sql
+++ aisdb/aisdb_sql/psql_static_trigger.sql
@@ -2,20 +2,25 @@ CREATE OR REPLACE FUNCTION static_insert()
 RETURNS TRIGGER AS $$
 DECLARE
     target_table TEXT;
+    cols TEXT;
+    vals TEXT;
 BEGIN
     -- Assuming NEW.time is the epoch timestamp in seconds
     target_table := lower('ais_' || TO_CHAR(TO_TIMESTAMP(NEW.time), 'YYYYMM') || '_static');
 
-    BEGIN
-        -- Check if the target table exists, if not, create it
-        IF NOT EXISTS (SELECT 1 FROM pg_tables WHERE tablename = lower(target_table)) THEN
-            EXECUTE 'CREATE TABLE ' || target_table || ' (LIKE ' || TG_TABLE_NAME || ' INCLUDING ALL)';
-        END IF;
-        -- Insert the new row into the correct table
-        EXECUTE 'INSERT INTO ' || target_table || ' VALUES (($1).*)' USING NEW;
-    END;
+    -- Creating the table if it does not exist
+    IF NOT EXISTS (SELECT 1 FROM pg_tables WHERE schemaname = 'public' AND tablename = target_table) THEN
+        EXECUTE format('CREATE TABLE public.%I (LIKE public.%I INCLUDING ALL)', target_table, TG_TABLE_NAME);
+    END IF;
 
-    -- Skip the original insert operation
+    -- Extracting columns and values from the NEW row as text
+    SELECT string_agg(quote_ident(key), ', ') INTO cols FROM each(hstore(NEW));
+    SELECT string_agg(quote_literal(value), ', ') INTO vals FROM each(hstore(NEW));
+
+    -- Dynamically building and executing the INSERT statement
+    EXECUTE format('INSERT INTO public.%I (%s) VALUES (%s)', target_table, cols, vals);
+
+    -- Skip the original INSERT operation
     RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
