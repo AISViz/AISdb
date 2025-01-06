f06456050d867575028e064777eb2351f5840281
ruixin <tsuzsrx@gmail.com>
2025-01-06 16:48:45 -0400

    Add test examples for inland denoising and cubic spline interpolation

---
+++ aisdb/tests/test_017_inland_denoising.py
@@ -0,0 +1,20 @@
+import os
+import numpy as np
+from aisdb.denoising_encoder import InlandDenoising
+
+y1, x1 = 44.30350650164017, -63.255341082253146
+y2, x2 = 44.29083001133964, -64.45527328316969  # Point (y2, x2) is inland
+y3, x3 = 44.10511509303621, -64.3325524220706
+
+data_dir = os.environ.get("AISDBDATADIR",
+                          os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "testdata", ), )
+
+tracks_short = [dict(lon=np.array([x1, x2, x3]), lat=np.array([y1, y2, y3]), time=[1, 2, 3], dynamic=set(["time"]), )]
+
+
+def test_inland_denoising():
+    cleaned_tracklist = []
+    with InlandDenoising(data_dir=data_dir) as remover:
+        cleaned_tracks = remover.filter_noisy_points(tracks_short)
+        cleaned_tracklist.extend(cleaned_tracks)
+        assert len(cleaned_tracklist) == len(tracks_short) - 1  # ensure the inland point is removed
