8b62bdc9beff7f1226d9e3a91dd786495c84ac7a
vaishnav <vaishnav@dal.ca>
2025-05-14 15:15:10 -0300

    refactor examples

--- aisdb/weather/data_store.py
+++ aisdb/weather/data_store.py
@@ -4,6 +4,11 @@ import types
 import xarray as xr
 import numpy as np
 from aisdb.database.decoder import fast_unzip
+from aisdb.weather.utils import SHORT_NAMES_TO_VARIABLES
+from aisdb.weather.weather_fetch import ClimateDataStore
+import cdsapi
+import os
+import shutil
 
 
 def dt_to_iso8601(timestamp):
@@ -59,7 +64,7 @@ def get_monthly_range(start,end) -> list:
     return months
 
 class WeatherDataStore:
-    def __init__(self,short_names: list, start:datetime.datetime,end: datetime.datetime, weather_data_path: str):
+    def __init__(self,short_names: list, start:datetime.datetime,end: datetime.datetime, weather_data_path: str, download_from_cds: bool = False,**kwargs):
         """
         Initialize a WeatherDataStore object to handle weather data extraction.
 
@@ -81,20 +86,41 @@ class WeatherDataStore:
         if not isinstance(short_names, list) or not all(isinstance(name, str) for name in short_names):
             raise ValueError("short_names should be a list of strings.")
         
-        # validate weather_data_path
+         # validate weather_data_path
         if weather_data_path =="":
             raise ValueError("WEATHER_DATA_PATH is not specified.")
         
-        for short_name in short_names:
-            if short_name not in available_short_names:
-                raise ValueError(f"Invalid short name: {short_name}")
-            
         self.start = start
         self.end = end
-        self.months = get_monthly_range(start, end)        
+        self.months = get_monthly_range(start, end) 
+
+        self._check_available_short_names(short_names)
+       
         self.short_names = short_names
+
+        if weather_data_path =="":
+            raise ValueError("WEATHER_DATA_PATH is not specified. WEATHER_DATA_PATH must be specified for either weather data extraction or to point to location where weather data grib file is placed")
+
         self.weather_data_path = weather_data_path
 
+        if download_from_cds == True:
+            self.area = kwargs.get("area")
+            if self.area  == None or len(self.area )==0:
+                raise ValueError("""Missing parameter 'area'.""")
+              
+            user_params = {
+                "short_names": self.short_names,
+                "start_time": self.start,
+                "end_time": self.end,
+                "area": self.area,
+            }
+
+            climateDataStore = ClimateDataStore(dataset="reanalysis-era5-single-levels", **user_params)
+
+            print(f"Downloading weather data from CDS to: {weather_data_path}")
+
+            climateDataStore.download_grib_file(output_folder=weather_data_path)
+        
         self.weather_ds = self._load_weather_data()
            
     def _load_weather_data(self):
@@ -110,15 +136,21 @@ class WeatherDataStore:
         # Create a temporary directory for extraction
         tmp_dir = tempfile.mkdtemp()
         zipped_grib_files = []
-        
+        grib_file_paths = []
+
         for month in self.months:
-            file_name = f"{self.weather_data_path}/{month}.grib"
+            grib_path = f"{self.weather_data_path}/{month}.grib"
+            zip_path = f"{grib_path}.zip"
 
-            # Unzip the GRIB file
-            zip_path = f"{file_name}.zip"
-            zipped_grib_files.append(zip_path)
+            if os.path.exists(zip_path):
+                zipped_grib_files.append(zip_path)
+            elif os.path.exists(grib_path):
+                shutil.copy(grib_path, f"{tmp_dir}/{month}.grib")
+            else:
+                raise FileNotFoundError(f"Neither {zip_path} nor {grib_path} found for month: {month}")
 
-        fast_unzip(zipped_grib_files, tmp_dir) # TODO: optimize to do in the same loop
+        if zipped_grib_files:
+            fast_unzip(zipped_grib_files, tmp_dir)
 
         for month in self.months:
             grib_file_path = f"{tmp_dir}/{month}.grib"
@@ -218,7 +250,6 @@ class WeatherDataStore:
             track["weather_data"] = weather_data_dict      
                   
             yield track
-        
 
 
     def close(self):
@@ -226,45 +257,15 @@ class WeatherDataStore:
         Close the weather dataset.
         """
         self.weather_ds.close()
+    
+    def _check_available_short_names(self, short_names):        
+        for short_name in short_names:
+                value =  SHORT_NAMES_TO_VARIABLES.get(short_name)
+                if value is None or value == "":
+                    raise ValueError(f"Invalid shortName: {short_name}.")
 
-available_short_names = {'tcc': True, 'inss': True, 'tciw': True, 'pev': True, 'lgws': True, 'ewss': True, 'mntpr': True, 'wss': True, 
-                         'w': True, 'mvtpm': True, 'msnswrfcs': True, 'viiwd': True, 'vst': True, 'cdir': True, 'mwp1': True, 
-                         'p1ww': True, 'msmr': True, 'iews': True, 'dwww': True, 'ptype': True, 'e': True, 'mwp2': True, 'mpww': True, 
-                         '2dfd': True, 'mttlwrcs': True, '2t': True, 'tsrc': True, 'msdrswrfcs': True, 'vitoee': True, 'tcrw': True, 
-                         'clwc': True, 'vithe': True, 'cvl': True, 'aluvp': True, 'istl2': True, '100v': True, 'ssr': True, 
-                         'tauoc': True, 'isor': True, 'mx2': True, 'swvl4': True, 'uvb': True, 'sshf': True, 'zust': True, 
-                         'msdwuvrf': True, 'pv': True, 'lai_lv': True, 'mdmf': True, 'viozn': True, 'vit': True, 'hcc': True, 
-                         'phiaw': True, 'msnlwrf': True, 'anor': True, 'sdor': True, 'sst': True, 'vitoen': True, 'tsr': True, 
-                         'vithed': True, 'mpts': True, 'mvimd': True, 'cin': True, 'sro': True, 'str': True, 'sf': True, 'src': True, 
-                         'mtdch': True, 'mttpm': True, 'ssrd': True, 'dndzn': True, 'vikee': True, 'mutpm': True, 'stl3': True, 
-                         'mtnswrfcs': True, 'mlspr': True, 'viozd': True, 'swh': True, 'mcsr': True, 'tclw': True, 'arrc': True, 
-                         'lict': True, 'tplt': True, 'mmtss': True, 'sd': True, 'crwc': True, 'o3': True, 'viiwn': True, 'wdw': True, 
-                         'mxtpr': True, 'ishf': True, 'vimat': True, 'ci': True, 'bld': True, 'hmax': True, 't': True, 'vike': True, 
-                         'ssrc': True, 'wsk': True, 'slor': True, 'nsss': True, 'mslhf': True, 'mtnswrf': True, 'viwvd': True, 
-                         'pres': True, 'smlt': True, 'flsr': True, 'msl': True, 'cswc': True, 'tco3': True, 'etadot': True, 
-                         'mumf': True, 'q': True, 'mudr': True, 'ttr': True, 'p2ww': True, 'istl3': True, 'mwd': True, 'msshf': True, 
-                         'mdww': True, 'viman': True, 'bfi': True, 'swh3': True, 'msqs': True, 'r': True, 'cvh': True, '10u': True, 
-                         'lmlt': True, 'lshf': True, 'vign': True, 'lssfr': True, 'mcpr': True, 'wstar': True, 'alnid': True, 
-                         '10fg': True, 'pt': True, 'strd': True, 'ssrdc': True, 'lsf': True, 'msdrswrf': True, 'swvl2': True, 
-                         'ttrc': True, 'vitoe': True, 'crr': True, 'cl': True, 'lcc': True, 'viwvn': True, 'vitoed': True, 'swh1': True, 
-                         'mp2': True, 'v': True, 'mp1': True, 'mqtpm': True, '100u': True, 'z': True, 'deg0l': True, 'asn': True, 
-                         'strc': True, 'vithen': True, 'vilwn': True, 'rhoao': True, 'mtpf': True, 'fsr': True, 'mdts': True, 'sp': True, 
-                         'slt': True, 'totalx': True, 'vimae': True, 'tcwv': True, 'mcc': True, 'vipie': True, 'swh2': True, 
-                         'slhf': True, 'mx2t': True, 'mddr': True, 'stl1': True, 'lblt': True, 'ie': True, 'msnlwrfcs': True, 
-                         'pp1d': True, 'dwi': True, 'awh': True, 'rsn': True, 'vo': True, 'mgwd': True, 'phioc': True, 'mttlwr': True, 
-                         'vithee': True, 'mser': True, 'metss': True, 'fdir': True, 'ciwc': True, 'tvl': True, 'lai_hv': True, 
-                         'swvl1': True, 'u10n': True, 'msdwlwrf': True, 'fal': True, 'dl': True, 'cp': True, 'mer': True, 'mwp': True, 
-                         'wsp': True, 'strdc': True, 'viwve': True, 'viken': True, 'dctb': True, 'vimad': True, '2d': True, 'lmld': True, 
-                         'vige': True, 'vigd': True, 'lsm': True, '10v': True, 'p1ps': True, 'mttswrcs': True, 'tmax': True, 'vimd': True, 
-                         'd': True, 'mbld': True, 'cc': True, 'sdfor': True, 'csf': True, 'dwps': True, 'mtpr': True, 'chnk': True, 
-                         'vilwd': True, 'mn2t': True, 'ust': True, 'mntss': True, 'tsn': True, 'msdwswrfcs': True, 'ssro': True, 
-                         'viked': True, 'cape': True, 'mttswr': True, 'ro': True, 'tp': True, 'vilwe': True, 'istl1': True, 'msror': True, 
-                         'dndza': True, 'cbh': True, 'ilspf': True, 'vipile': True, 'tcsw': True, 'mssror': True, 'mngwss': True, 
-                         'mtnlwrfcs': True, 'mwd1': True, 'msr': True, 'mror': True, 'mwp3': True, 'tplb': True, 'ltlt': True, 
-                         'mont': True, 'aluvd': True, 'v10n': True, 'mtnlwrf': True, 'stl2': True, 'mwd2': True, 'tcw': True, 'mper': True, 
-                         'blh': True, 'msnswrf': True, 'p2ps': True, 'magss': True, 'msdwswrf': True, 'swvl3': True, 'mlspf': True, 
-                         'mwd3': True, 'shts': True, 'msdwlwrfcs': True, 'mlssr': True, 'lsp': True, 'tvh': True, 'lsrr': True, 
-                         'stl4': True, 'i10fg': True, 'vioze': True, 'viiwe': True, 'kx': True, 'istl4': True, 'mgws': True, 'cdww': True, 
-                         '10si': True, 'tcslw': True, 'viec': True, 'megwss': True, 'tisr': True, 'mtdwswrf': True, 'alnip': True, 
-                         'es': True, 'wind': True, 'u': True, 'lspf': True, 'wmb': True, 'acwh': True, 'lnsp': True, 'vima': True, 
-                         'licd': True, 'shww': True, 'csfr': True, 'skt': True, 'gwd': True}
+DEFAULT_PARAMS = {
+    "product_type": ["reanalysis"],
+    "data_format": "grib",
+    "download_format": "unarchived",
+}
