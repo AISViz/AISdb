c42d147fbce0599a26ae6a04e37dfcc6fbf8d18a
Ruixin Song <tsuzsrx@gmail.com>
2025-06-18 12:04:08 -0300

    Update test_015_raster_dist.py

--- aisdb/tests/test_015_raster_dist.py
+++ aisdb/tests/test_015_raster_dist.py
@@ -9,6 +9,10 @@ y1, x1 = 48.271185186388735, -61.10595523571155
 data_dir = os.environ.get("AISDBDATADIR",
                           os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "testdata", ), )
 
+# Print to log the path being used
+print(f"[DEBUG] data_dir is: {data_dir}")
+assert os.path.isdir(data_dir), f"Data directory does not exist: {data_dir}"
+
 tracks_short = [dict(lon=np.array([x1]), lat=np.array([y1]), time=[0], dynamic=set(["time"]), )]
 
 
